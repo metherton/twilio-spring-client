aaebf7dbb35dc44e912c3a052fd83cdc5207ca5f
metherton <martinetherton63@googlemail.com>
2015-11-22 20:08:43 +0100

    first commit

---
+++ src/main/java/com/martinetherton/twilio/client/TwilioSpringClientApplication.java
@@ -0,0 +1,65 @@
+package com.martinetherton.twilio.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+public class TwilioSpringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwilioSpringClientApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+                    .anyRequest().authenticated().and()
+                    .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class).csrf().csrfTokenRepository(csrfTokenRepository());;
+        }
+
+        private CsrfTokenRepository csrfTokenRepository() {
+            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+            repository.setHeaderName("X-XSRF-TOKEN");
+            return repository;
+        }
+
+    }
+
+    @RequestMapping("/token")
+    @ResponseBody
+    public Map<String,String> token(HttpSession session) {
+        return Collections.singletonMap("token", session.getId());
+    }
+
+}
---
+++ src/test/java/com/martinetherton/twilio/client/TwilioSpringClientApplicationTests.java
@@ -0,0 +1,18 @@
+package com.martinetherton.twilio.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TwilioSpringClientApplication.class)
+@WebAppConfiguration
+public class TwilioSpringClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
